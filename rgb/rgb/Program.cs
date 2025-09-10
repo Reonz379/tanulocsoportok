b7f31e1d73ce14608c5abc2f084d1eead70ded0b
HzkMarton <160259662+HzkMarton@users.noreply.github.com>
2025-09-10 11:06:15 +0200

    Add files via upload
    RGB feladat

---
+++ rgb/rgb/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace rgb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<RGB[]> colors = new List<RGB[]>();
+            try
+            {
+                foreach (string line in File.ReadAllLines("kep.txt"))
+                {
+                    string[] nums = line.Split(' ');
+                    RGB[] rgb = new RGB[640];
+                    int j = 0;
+                    for (int i = 0; i < nums.Length; i += 3)
+                    {
+                        rgb[j] = new RGB(int.Parse(nums[i]), int.Parse(nums[i + 1]), int.Parse(nums[i + 2]));
+                        j++;
+                    }
+                    colors.Add(rgb);
+                }
+                RGB.Lekerdezes(colors);
+                Console.WriteLine($"3. feladat:\nA világos képpontok száma: {RGB.Vilagosok}");
+                RGB.Legsotetebbek(colors);
+                Console.WriteLine("6. feladat:");
+                for (int i = 0; i < colors.Count; i++) {
+                    bool meghaladja = RGB.hatar(colors, i, 10);
+                    if (meghaladja)
+                    {
+                        Console.WriteLine($"A felhő legfelső sora: {i+1}");
+                        break;
+                    }
+                }
+                for (int i = colors.Count-1; i >-1 ; i--)
+                {
+                    bool meghaladja = RGB.hatar(colors, i, 10);
+                    if (meghaladja)
+                    {
+                        Console.WriteLine($"A felhő legalsó sora: {i+1}");
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
---
+++ rgb/rgb/RGB.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rgb
+{
+    internal class RGB
+    {
+        private int _r;
+        private int _g;
+        private int _b;
+        private int _rgbt;
+        static public int Vilagosok;
+        static public int Sotet = 10000;
+        public RGB(int r, int g, int b)
+        {
+            this._r = r;
+            this._g = g;
+            this._b = b;
+            this._rgbt = _r+_g+_b;
+            if (_rgbt > 600) 
+            {
+                Vilagosok++;
+            }
+            if (this._rgbt < Sotet)
+            {
+                Sotet = this._rgbt ;
+                
+            }
+        }
+        public int R { get { return _r; } }
+        public int G { get { return _g; } }
+        public int B { get { return _b; } }
+        public int RGBT { get { return _rgbt; } } 
+
+        public static void Lekerdezes(List<RGB[]> colors)
+        {
+            Console.Write($"2. feladat:\nKérem egy képpont adatait!\nSor:");
+            int sor = 0;
+            int oszlop = 0;
+            if (int.TryParse(Console.ReadLine(), out int s)) sor = s;
+            Console.Write("Oszlop:");
+            if (int.TryParse(Console.ReadLine(), out int o)) oszlop = o;
+            Console.WriteLine($"A képpont színe RGB({colors[sor-1][oszlop-1].R},{colors[sor - 1][oszlop - 1].G},{colors[sor - 1][oszlop - 1].B})");
+        }
+        public static void Legsotetebbek(List<RGB[]> colors)
+        {
+            Console.WriteLine($"4. feladat:\nA legsötétebb pont RGB összege: {RGB.Sotet}\nA legsötétebb pixelek színe:");
+            List<RGB> rgbs = new List<RGB>();
+            foreach(RGB[] rgb in colors)
+            {
+                foreach (RGB item in rgb)
+                {
+                    if(item.RGBT == RGB.Sotet&&!rgbs.Contains(item))
+                    {
+                        rgbs.Add(item);
+                    }
+                }
+            }
+            foreach(RGB item in rgbs) Console.WriteLine($"RGB({item.R},{item.G},{item.B})");
+        }
+        public static bool hatar(List<RGB[]> colors, int sorszam, int elteres)
+        {
+            bool elter = false;
+            RGB[] sor = colors[sorszam];
+            for(int i = 0; i < sor.Length-1; i++)
+            {
+                if (sor[i].B - sor[i+1].B > elteres || sor[i+1].B - sor[i].B > elteres)
+                {
+                    elter = true;
+                    break;
+                }
+            }
+            return elter;
+        }
+    }
+}
